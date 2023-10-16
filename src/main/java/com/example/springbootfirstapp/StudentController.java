6a88139facb45087c1193852ccfc07a570b0817c
Hamza Mekni <hamzamekni4@gmail.com>
2023-10-16 19:10:56 +0100

    TP2SpringBoot third Commit

--- src/main/java/com/example/springbootfirstapp/StudentController.java
+++ src/main/java/com/example/springbootfirstapp/StudentController.java
@@ -1,20 +1,18 @@
 package com.example.springbootfirstapp;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudentController {
-@GetMapping("/students")
-public List<Student> getStudents(){
-List<Student> students = new ArrayList<>();
-students.add(new Student("Ramesh", "Fadatare"));
-students.add(new Student("Tony", "Cena"));
-students.add(new Student("Sanjay", "Jadhav"));
-students.add(new Student("Ram", "Jadhav"));
-students.add(new Student("Umesh", "Fadatare"));
-return students;
+// http://localhost:8080/student/1
+// @PathVariable annotation
+@GetMapping("/student/{firstName}/{lastName}/")
+public Student studentPathVariable(@PathVariable("firstName") String
+firstName,
+@PathVariable("lastName") String lastName) {
+return new Student(firstName, lastName);
 }
 }
+
 
