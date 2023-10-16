53e6206e070de31da0e92b0f28dab48faf3fd0c4
Hamza Mekni <hamzamekni4@gmail.com>
2023-10-16 19:11:55 +0100

    TP2SpringBoot fourth Commit

--- src/main/java/com/example/springbootfirstapp/StudentController.java
+++ src/main/java/com/example/springbootfirstapp/StudentController.java
@@ -1,18 +1,19 @@
 package com.example.springbootfirstapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudentController {
-// http://localhost:8080/student/1
-// @PathVariable annotation
-@GetMapping("/student/{firstName}/{lastName}/")
-public Student studentPathVariable(@PathVariable("firstName") String
-firstName,
-@PathVariable("lastName") String lastName) {
+// build rest API to handle query parameters
+//http://localhost:8080/student/query?firstName=Ramesh&lastName=Fadatare
+@GetMapping("/student/query")
+public Student studentQueryParam(
+@RequestParam(name = "firstName") String firstName,
+@RequestParam(name = "lastName") String lastName) {
 return new Student(firstName, lastName);
 }
 }
+
 
 
