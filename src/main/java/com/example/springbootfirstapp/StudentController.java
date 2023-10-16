d6c99725e078074bbbfae000da52c641c26fd479
Hamza Mekni <hamzamekni4@gmail.com>
2023-10-16 19:09:19 +0100

    TP2SpringBoot second Commit

--- src/main/java/com/example/springbootfirstapp/StudentController.java
+++ src/main/java/com/example/springbootfirstapp/StudentController.java
@@ -1,14 +1,20 @@
 package com.example.springbootfirstapp;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
 @RestController
 public class StudentController {
-// http://localhost:8080/student
-@GetMapping("/student")
-public Student getStudent() {
-return new Student("Ramesh", "Fadatare");
+@GetMapping("/students")
+public List<Student> getStudents(){
+List<Student> students = new ArrayList<>();
+students.add(new Student("Ramesh", "Fadatare"));
+students.add(new Student("Tony", "Cena"));
+students.add(new Student("Sanjay", "Jadhav"));
+students.add(new Student("Ram", "Jadhav"));
+students.add(new Student("Umesh", "Fadatare"));
+return students;
 }
 }
+
